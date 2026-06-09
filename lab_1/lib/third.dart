bc94b915c5fa899c2d83aed45e3fdf53757425e6
Maanas1815 <your-email-used-on-github>
2026-06-09 13:14:40 +0530

    3 programs done

--- lab_1/lib/third.dart
+++ lab_1/lib/third.dart
@@ -17,5 +17,21 @@ int div(a, b) {
 }
 
 void main() {
-  print("Enter 1 for Add 2 for Subtract 3 for Multiplication 4 for Division")
+  print("Enter 1 for Add 2 for Subtract 3 for Multiplication 4 for Division");
+  int inp = int.parse(stdin.readLineSync()!);
+  print("Enter Number 1 : ");
+  int a = int.parse(stdin.readLineSync()!);
+  print("Enter Number 2 : ");
+  int b = int.parse(stdin.readLineSync()!);
+
+  if (inp == 1)
+    print(add(a, b));
+  else if (inp == 2)
+    print(subtract(a, b));
+  else if (inp == 3)
+    print(mlt(a, b));
+  else if (inp == 4)
+    print(div(a, b));
+  else
+    print("Please enter valid number");
 }
